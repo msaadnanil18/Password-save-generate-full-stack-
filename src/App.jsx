b6b82e34e547314c247433294fc986d0d450c041
msaadnanil18 <140881151+msaadnanil18@users.noreply.github.com>
2023-12-23 10:58:04 +0000

    add ui

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,42 @@ import viteLogo from '/vite.svg'
 import { Button, Card, Input, Form } from 'antd'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+  const [inputValue , setInputValue ] = useState('2222')
+   const [form] = Form.useForm()
+form.setFieldsValue({
+    password: inputValue ,
+  });
+ return (
     <>
-   
-    <div className=' grid place-content-center h-screen ' >
-      <div className='m-10' >
-      <h1 className=' text-slate-200  ' >Password Generator</h1>
+ <div className=' grid place-content-center h-screen ' >
+    <div className='my-5 grid place-content-center' >
+      <h1 className=' text-slate-200 text-4xl ' >Password Generator</h1>
       </div>
-   
-    <Card
+     <Card
     className=' p-4 w-96'
     hoverable
-    
-    >
+   >
       <Form
       layout='vertical'
+      form={form}
       >
         <Form.Item name='fieldPassword' label='Enter field' rules={[{required:true, message:"Please enter field "}]} >
           <Input placeholder='Enter field of password' />
          </Form.Item>
          <Form.Item name='password' label='Password' >
-          <Input />
+         <Input />
+         </Form.Item>
+
+         <Form.Item>
+          <Button>
+            Click here to copy & submit
+          </Button>
          </Form.Item>
 
       </Form>
 
     </Card>
+    
     </div>
        
     </>
@@ -39,3 +47,48 @@ function App() {
 }
 
 export default App
+
+
+
+// import { useState, useEffect } from 'react';
+
+// import { Button, Card, Input, Form } from 'antd';
+
+// function App() {
+//   const [form] = Form.useForm(); // Access the form instance
+
+//   useEffect(() => {
+//     // Set the initial value for the field inside the form
+ 
+//   }, [form]);
+
+//   return (
+//     <>
+//       <div className='grid place-content-center h-screen'>
+//         <div className='my-5 grid place-content-center'>
+//           <h1 className='text-slate-200 text-4xl'>Password Generator</h1>
+//         </div>
+//         <Card className='p-4 w-96' hoverable>
+//           <Form
+//             layout='vertical'
+//             form={form} // Pass the form instance
+//           >
+//             <Form.Item
+//               name='fieldPassword'
+//               label='Enter field'
+//               rules={[{ required: true, message: 'Please enter field' }]}
+//             >
+//               <Input placeholder='Enter field of password' />
+//             </Form.Item>
+//             <Form.Item name='password' label='Password'>
+//               <Input />
+//             </Form.Item>
+//           </Form>
+//         </Card>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default App;
+
