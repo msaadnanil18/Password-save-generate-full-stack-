a9dc106971939a50f5e5154c127ad75d6ce6d0ba
msaadnanil18 <140881151+msaadnanil18@users.noreply.github.com>
2023-12-23 10:37:23 +0000

    add ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from 'antd'
+import { Button, Card, Input, Form } from 'antd'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <div className=' bg-black'>
-   <h1 className=' bg-gray-950 text-slate-300' >hloo</h1>
-      hlo world
-      <Button>hloo</Button>
-     </div>
+   
+    <div className=' grid place-content-center h-screen ' >
+      <div className='m-10' >
+      <h1 className=' text-slate-200  ' >Password Generator</h1>
+      </div>
+   
+    <Card
+    className=' p-4 w-96'
+    hoverable
+    
+    >
+      <Form
+      layout='vertical'
+      >
+        <Form.Item name='fieldPassword' label='Enter field' rules={[{required:true, message:"Please enter field "}]} >
+          <Input placeholder='Enter field of password' />
+         </Form.Item>
+         <Form.Item name='password' label='Password' >
+          <Input />
+         </Form.Item>
 
-        
+      </Form>
+
+    </Card>
+    </div>
+       
     </>
   )
 }
